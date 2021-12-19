56c1a428a72b65a94f2553437fc8a7977229401f
Gregorius Albert <galbert.djaja@gmail.com>
2021-12-19 17:14:37 +0700

    Initial Commit

---
+++ Cryptomate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptomate
+//
+//  Created by Gregorius Albert on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
